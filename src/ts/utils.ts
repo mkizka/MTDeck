d4f18619f62450a4ed82f5edc90ed2fe8dff0748
Compeito <com0806peito@icloud.com>
2019-11-04 11:43:52 +0900

    言語の判定処理修正

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -14,6 +14,6 @@ export const _ = (messageName: string): string => {
   if (typeof chrome !== 'undefined') {
     return chrome.i18n.getMessage(messageName);
   }
-  const lang = window.navigator.language === 'ja' ? 'ja' : 'en';
+  const lang = /ja/.test(window.navigator.language) ? 'ja' : 'en';
   return require(`../_locales/${lang}/messages.json`)[messageName].message || '';
 };
