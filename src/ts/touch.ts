736a6f08252d01e54cbf016a94f13364f406dafb
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-07 00:22:13 +0900

    yarn format実行

--- src/ts/touch.ts
+++ src/ts/touch.ts
@@ -15,7 +15,7 @@ export class TouchManager {
     );
   }
   constructor($element: HTMLElement) {
-    $element.addEventListener("click", () => this.onTap())
+    $element.addEventListener("click", () => this.onTap());
     $element.addEventListener("touchstart", (e) => {
       this.start = {
         x: e.touches[0].screenX,
