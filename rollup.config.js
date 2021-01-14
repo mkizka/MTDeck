dc68be60d5e3bca2be866b6ff1e776fd6690535b
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-14 13:43:55 +0900

    terserがuserscriptHeaderより前に来るように

--- rollup.config.js
+++ rollup.config.js
@@ -5,48 +5,52 @@ import userscriptHeader from "rollup-plugin-userscript-header";
 import copy from "rollup-plugin-copy";
 import pkg from "./package.json";
 
+export const bundlePlugins = [
+  typescript(),
+  json(),
+  sass({
+    insert: true,
+  }),
+];
+
+export const userscriptPlugin = userscriptHeader({
+  overwrite: {
+    name: "MTDeck",
+    match: "https://tweetdeck.twitter.com",
+  },
+});
+
+const copyPlugin = copy({
+  targets: [
+    {
+      src: "./src/manifest.json",
+      transform: (buffer) => {
+        const manifest = JSON.parse(buffer.toString());
+        manifest.version = pkg.version;
+        manifest.developer = {
+          name: pkg.author,
+          url: pkg.homepage,
+        };
+        return JSON.stringify(manifest, null, 2);
+      },
+      dest: "./dist",
+    },
+    {
+      src: "./src/icons",
+      dest: "./dist",
+    },
+    {
+      src: "./src/_locales",
+      dest: "./dist",
+    },
+  ],
+});
+
 export default {
   input: "./src/index.ts",
   output: {
     file: "./dist/mtdeck.user.js",
     format: "iife",
   },
-  plugins: [
-    typescript(),
-    json(),
-    sass({
-      insert: true,
-    }),
-    userscriptHeader({
-      overwrite: {
-        name: "MTDeck",
-        match: "https://tweetdeck.twitter.com",
-      },
-    }),
-    copy({
-      targets: [
-        {
-          src: "./src/manifest.json",
-          transform: (buffer) => {
-            const manifest = JSON.parse(buffer.toString());
-            manifest.version = pkg.version;
-            manifest.developer = {
-              name: pkg.author,
-              url: pkg.homepage,
-            };
-            return JSON.stringify(manifest, null, 2);
-          },
-          dest: "./dist",
-        },
-        {
-          src: "./src/icons",
-          dest: "./dist",
-        },
-        {
-          src: "./src/_locales",
-          dest: "./dist",
-        },
-      ],
-    }),
-  ],
+  plugins: [...bundlePlugins, userscriptPlugin, copyPlugin],
 };
--- rollup.config.minify.js
+++ rollup.config.minify.js
@@ -1,11 +1,7 @@
-import config from "./rollup.config";
 import { terser } from "rollup-plugin-terser";
+import config, { bundlePlugins, userscriptPlugin } from "./rollup.config";
 
-config.output = {
-  file: "dist/mtdeck.min.user.js",
-  format: "iife",
-};
-
-config.plugins = [...config.plugins, terser()];
+config.output.file = "dist/mtdeck.min.user.js";
+config.plugins = [...bundlePlugins, terser(), userscriptPlugin];
 
 export default config;
