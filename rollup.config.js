4315ad5a13114a7c6aad4c2b4e8bc5b931598266
mkizka <30231179+mkizka@users.noreply.github.com>
2021-01-14 13:12:00 +0900

    各種ファイルをコピーするように

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import sass from "rollup-plugin-sass";
 import userscriptHeader from "rollup-plugin-userscript-header";
+import copy from "rollup-plugin-copy";
+import pkg from "./package.json";
 
 export default {
-  input: "src/index.ts",
+  input: "./src/index.ts",
   output: {
-    file: "dist/mtdeck.user.js",
+    file: "./dist/mtdeck.user.js",
     format: "iife",
   },
   plugins: [
@@ -20,6 +22,31 @@ export default {
         name: "MTDeck",
         match: "https://tweetdeck.twitter.com",
       },
+    }),
+    copy({
+      targets: [
+        {
+          src: "./src/manifest.json",
+          transform: (buffer) => {
+            const manifest = JSON.parse(buffer.toString());
+            manifest.version = pkg.version;
+            manifest.developer = {
+              name: pkg.author,
+              url: pkg.homepage,
+            };
+            return JSON.stringify(manifest, null, 2);
+          },
+          dest: "./dist",
+        },
+        {
+          src: "./src/icons",
+          dest: "./dist/icons",
+        },
+        {
+          src: "./src/_locales",
+          dest: "./dist/_locales",
+        },
+      ],
     }),
   ],
 };
